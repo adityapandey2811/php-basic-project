810e9b03123f9c0e7b074cbed6ced162d8673588
Aditya <2811adityapandey@gmail.com>
2022-11-19 21:55:53 +0530

    Updated index.php

--- index.php
+++ index.php
@@ -1,3 +1,68 @@
 <?php
-    
+    $insert = false;
+    if(isset($_POST['name'])) {
+        $server = "localhost";
+        $username = "root";
+        $password = "";
+
+        $con = mysqli_connect($server, $username, $password);
+
+        if (!$con) {
+            die("Connection failed: " . mysqli_connect_error());
+        }
+        // echo "Connection Successful";
+
+        $name = $_POST['name'];
+        $gender = $_POST['gender'];
+        $age = $_POST['age'];
+        $email = $_POST['email'];
+        $phone = $_POST['phone'];
+        $desc = $_POST['desc'];
+
+        $sql = "INSERT INTO `trip`.`trip` (`name`, `age`, `gender`, `email`, `phone`, `other`, `dt`) VALUES ('$name', '$age', '$gender', '$email', '$phone', '$desc', current_timestamp());";
+
+        if($con->query($sql) == true){
+            // echo "Successfully Inserted";
+            $insert = true;
+        }
+        else{
+            echo "ERROR: $sql <br> $con->error";
+        }
+        $con->close();
+    }
+
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welcome to Travel Form</title>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@200;400&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <img class="bg" src="bg.jpg" alt="" class="KIIT University">
+    <div class="container">
+        <h1>Welcome to KIIT University</h1>
+        <p>Enter Details</p>
+        <?php
+            if($insert == true){
+                echo "<p class='submitMsg'>Thanks for submitting your form. We are happy to see you joining us for the trip</p>";
+            }
+        ?>
+        <form action="index.php" method="post">
+            <input type="text" name="name" id="name" placeholder="Enter your name">
+            <input type="text" name="age" id="age" placeholder="Enter your age">
+            <input type="text" name="gender" id="gender" placeholder="Enter your gender">
+            <input type="text" name="email" id="email" placeholder="Enter your email">
+            <input type="text" name="phone" id="phone" placeholder="Enter your phone">
+            <textarea name="desc" id="desc" cols="30" rows="10" placeholder="Enter any other information here"></textarea>
+            <button class="btn">Submit</button>
+            <!-- <button class="btn">Reset</button> -->
+        </form>
+    </div>
+    <script src="index.js"></script>
+</body>
+</html>
